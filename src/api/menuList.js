39bb4c73b9f02fbd2bba6d1a24c9fc46515d341b
wangzhaoyang <wzywangzhao@163.com>
2020-06-21 17:42:27 +0800

    增加菜单管理分页

--- src/api/menuList.js
+++ src/api/menuList.js
@@ -5,4 +5,18 @@ export function menuList(params) {
       method:'get',
       params
     })
-  }
+}
+export function addMenu(params) {
+    return request({
+      url: '/menu/addMenu',
+      method:'get',
+      params
+    })
+}
+export function menuListPage(params){
+  return request({
+    url:'/menu/menuListByPage',
+    method:'get',
+    params
+  })
+}
--- src/components/user/cdgl.vue
+++ src/components/user/cdgl.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cdgl-list">
     <div class="addmenu">
-      <el-button type="primary" icon="el-icon-plus" size="mini" @click="addParentMenu">添加菜单</el-button>
+      <el-button type="primary" icon="el-icon-plus" size="mini" @click="openMenu(0)">添加菜单</el-button>
     </div>
     <el-table :data="menuList" style="width: 100%" border stripe>
       <!--展开列-->
@@ -28,7 +28,12 @@
       <el-table-column label="操作" width="320">
         <template slot-scope="scope">
           <!--作用域插槽-->
-          <el-button type="primary" icon="el-icon-plus" size="mini" @click="addChildMenu">添加子菜单</el-button>
+          <el-button
+            type="primary"
+            icon="el-icon-plus"
+            size="mini"
+            @click="openMenu(scope.row.id)"
+          >添加子菜单</el-button>
           <el-button type="primary" size="mini" icon="el-icon-edit">修改</el-button>
           <el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
         </template>
@@ -36,35 +41,43 @@
     </el-table>
     <!--对话框-->
     <el-dialog :title="titleDia" :visible.sync="dialogFormVisible" width="25%">
-      <el-form :model="form">
+      <el-form :model="addForm">
         <el-form-item label="菜单名称" :label-width="formLabelWidth">
-          <el-input v-model="form.name"></el-input>
+          <el-input v-model="addForm.name"></el-input>
         </el-form-item>
         <el-form-item label="角色" :label-width="formLabelWidth">
-          <el-select v-model="form.region" placeholder="请选择角色">
+          <el-select v-model="addForm.roles" placeholder="请选择角色" multiple>
             <el-option label="管理员" value="admin"></el-option>
             <el-option label="教师" value="teacher"></el-option>
             <el-option label="学生" value="student"></el-option>
           </el-select>
         </el-form-item>
 
-        <el-form-item label="父菜单" v-if="parqntMenuShow" :label-width="formLabelWidth">
-          <el-input v-model="form.parentmenu"></el-input>
-        </el-form-item>
         <el-form-item label="前端路由" :label-width="formLabelWidth">
-          <el-input v-model="form.component"></el-input>
+          <el-input v-model="addForm.component"></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+        <el-button type="primary" @click="addNewMenu">确 定</el-button>
       </div>
     </el-dialog>
+    <div class="block">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page.sync="currentPage"
+        :page-sizes="pageSizes"
+        :page-size="pageSize"
+        layout="sizes, prev, pager, next"
+        :total="total"
+      ></el-pagination>
+    </div>
   </div>
 </template>
 
 <script>
-import { menuList } from "../../api/menuList";
+import { menuList, addMenu,menuListPage } from "../../api/menuList";
 export default {
   data() {
     return {
@@ -77,12 +90,16 @@ export default {
       ],
       parqntMenuShow: false,
       formLabelWidth: "80px",
-      form: {
+      titleDia: "",
+      pageSizes: [5, 10, 15, 20],
+      pageSize: 5,
+      total:0,
+      currentPage: 1,
+      addForm: {
         name: "",
-        role: "",
-        parentmenu: "",
+        roles: [],
         component: "",
-        titleDia: ""
+        parentId: 0
       }
     };
   },
@@ -92,31 +109,87 @@ export default {
   methods: {
     getMenuList() {
       let params = {
+        pageNum:this.currentPage,
+        pageSize:this.pageSize,
         role: "admin"
       };
-      menuList(params).then(response => {
-        this.menuList = response.data.menuList;
+      menuListPage(params).then(response => {
+        this.menuList = response.data.menuList.list
+        this.total=response.data.menuList.total
         this.menuList.forEach(item => {
-          item.userRole = this.roleList.filter(item1 => {
-            return item.userRole == item1.value;
-          })[0].name;
-          item.children.forEach(item2 => {
-            item2.userRole = this.roleList.filter(item3 => {
-              return item2.userRole == item3.value;
+          if (item.userRole.indexOf(",") == -1) {
+            item.userRole = this.roleList.filter(item1 => {
+              return item.userRole == item1.value;
             })[0].name;
+          } else {
+            var roleMul = item.userRole.split(",");
+            roleMul.forEach((mul, i) => {
+              roleMul[i] = this.roleList.filter(itemMul => {
+                return mul == itemMul.value;
+              })[0].name;
+            });
+            item.userRole = roleMul.toString();
+          }
+          item.children.forEach(item2 => {
+            if (item2.userRole.split(",") == -1) {
+              item2.userRole = this.roleList.filter(item3 => {
+                return item2.userRole == item3.value;
+              })[0].name;
+            } else {
+              var roleChildMul = item2.userRole.split(",");
+              roleChildMul.forEach((childMul, i) => {
+                roleChildMul[i] = this.roleList.filter(itemChildMul => {
+                  return childMul == itemChildMul.value;
+                })[0].name;
+              });
+              item2.userRole = roleChildMul.toString();
+            }
           });
         });
       });
     },
-    addParentMenu() {
-      this.parqntMenuShow = false;
-      this.dialogFormVisible = true;
-      this.titleDia = "添加菜单";
-    },
-    addChildMenu() {
+    openMenu(val) {
+      this.addForm.name = "";
+      this.addForm.roles = [];
+      this.addForm.component = "";
       this.parqntMenuShow = true;
       this.dialogFormVisible = true;
-      this.titleDia = "添加子菜单";
+      this.addForm.parentId = val;
+      if (val == 0) {
+        this.titleDia = "添加菜单";
+      } else {
+        this.titleDia = "添加子菜单";
+      }
+    },
+    addNewMenu() {
+      this.dialogFormVisible = false;
+      var rolesSave = [];
+      this.addForm.roles.forEach(item => {
+        rolesSave.push(item);
+      });
+      let params = {
+        menuname: this.addForm.name,
+        userrole: rolesSave.toString(),
+        parentId: this.addForm.parentId,
+        component: this.addForm.component
+      };
+      addMenu(params).then(response => {
+        if (response.data == 1) {
+          this.$toast("添加成功");
+          this.getMenuList(); //控制表格刷新
+          this.$parent.$parent.$parent.$parent.getMenuList(); //控制左侧导航栏刷新
+        } else {
+          this.$toast("添加失败");
+        }
+      });
+    },
+    handleSizeChange(val){
+      this.pageSize=val
+      this.getMenuList()
+    },
+    handleCurrentChange(val){
+      this.currentPage=val
+      this.getMenuList()
     }
   }
 };
@@ -127,7 +200,7 @@ export default {
   margin-left: 0;
   margin-bottom: 10px;
 }
-.cdgl-list /deep/ .el-input__inner {
+.cdgl-list /deep/ .el-form .el-input__inner {
   width: 220px;
 }
 </style>
